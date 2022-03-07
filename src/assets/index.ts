a2d59b9c6e258085efd829760270c0ca7c21f82c
osama <osamagamal345@gmail.com>
2022-03-08 00:23:20 +0200

    installing fonts && adding assets

---
+++ src/assets/index.ts
@@ -0,0 +1,11 @@
+export const theme = {
+  // colors
+  colorWhite: '#fff',
+  colorBlue: '#38b1e5',
+  colorGreen: '#30e1a2',
+};
+//fonts
+export const fonts = {
+  fontBold: 'Lato-Bold',
+  fontRegular: 'Lato-Regular',
+};
