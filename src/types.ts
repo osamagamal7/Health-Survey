7185c515f600d5efc1328dcb74551ed92dd99327
osama <osamagamal345@gmail.com>
2022-03-08 20:36:56 +0200

    including types of the data

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,22 @@
+export type HealthRangeWithTimestamp = {
+  sleepQualityRange: number;
+  skinQualityRange: number;
+  timestamp: number;
+};
+
+export type AppData = {
+  data: HealthRangeWithTimestamp[];
+};
+
+export type AppContextType = {
+  healthRangeList: AppData['data'];
+  handleSelectedRange: (
+    sleepQualityRange: number,
+    skinQualityRange: number,
+  ) => void;
+  handleDeleteRecord: (record: HealthRangeWithTimestamp) => void;
+};
+
 //Navigation Types
 export type MainBottomTabParamList = {
   Home: undefined;
